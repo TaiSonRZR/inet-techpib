28b6724a6f2f89eabe670eefec98bf8c36689519
TaiSonRZR <77694156+TaiSonRZR@users.noreply.github.com>
2024-03-29 02:53:14 +0100

    added database connection

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongoose = require("mongoose");
+//const uri = process.env.DATABASE_URL;
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// DATABASE CONNECTION AND SETTINGS
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error());
+db.once("open", () => console.log("Connected to database"));
+
+//app.get("/", (req, res) => res.send("Express on Vercel"));
+
+// SENDING FRONTEND
+
+app.use(express.static("public"));
+
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-export default app;
+module.exports = app;
