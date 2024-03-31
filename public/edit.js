bbb8d6f3bc40e10cb7c40e7c6eee7b8a8d7e6610
TaiSonRZR <77694156+TaiSonRZR@users.noreply.github.com>
2024-03-31 18:58:32 +0200

    added delete Button for every element

--- public/edit.js
+++ public/edit.js
@@ -2,19 +2,36 @@ document.getElementById("back").addEventListener("click", function () {
   window.location.href = "adminpanel.html";
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetch("http://localhost:3000/questions/")
-      .then(response => {
-          if (!response.ok) {
-              throw new Error("Network response was not ok");
-          }
-          return response.json();
-      })
-      .then(data => {
-          //displayData(data);
-          console.log(data);
-      })
-      .catch(error => {
-          console.error("Error fetching data:", error);
-      });
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      displayData(data);
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
 });
+
+function displayData(data) {
+  const list = document.getElementById("question-list");
+  data.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `<strong>${item.title}</strong>: ${
+      item.questionText
+    } - Antwort: ${item.answer ? "Ja" : "Nein"}`;
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Löschen";
+    deleteButton.addEventListener("click", function () {
+      deleteItem(item); // Ruft die Funktion deleteItem auf, um das Element zu löschen
+    });
+    listItem.appendChild(deleteButton);
+    list.appendChild(listItem);
+  });
+}
