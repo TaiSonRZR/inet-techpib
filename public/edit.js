e9608e36d1f0b29fc2252225cc3a588bffe55dbe
TaiSonRZR <77694156+TaiSonRZR@users.noreply.github.com>
2024-03-31 18:50:22 +0200

    fetch all questions for edit

--- public/edit.js
+++ public/edit.js
@@ -1,3 +1,20 @@
 document.getElementById("back").addEventListener("click", function () {
   window.location.href = "adminpanel.html";
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("http://localhost:3000/questions/")
+      .then(response => {
+          if (!response.ok) {
+              throw new Error("Network response was not ok");
+          }
+          return response.json();
+      })
+      .then(data => {
+          //displayData(data);
+          console.log(data);
+      })
+      .catch(error => {
+          console.error("Error fetching data:", error);
+      });
+});
