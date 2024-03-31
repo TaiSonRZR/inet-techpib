21f4b05d56b66f398b0c8ef5fbdbff905bcbced7
TaiSonRZR <77694156+TaiSonRZR@users.noreply.github.com>
2024-03-31 18:22:56 +0200

    Add question function and form handler

--- public/add.js
+++ public/add.js
@@ -1,3 +1,20 @@
 document.getElementById("back").addEventListener("click", function () {
   window.location.href = "adminpanel.html";
 });
+
+let form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  let formData = new FormData(form);
+  let data = Object.fromEntries(formData);
+  let jsonData = JSON.stringify(data);
+  event.preventDefault();
+  fetch("https://long-red-housecoat.cyclic.app/questions/", {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json'
+    },
+    body: jsonData
+  });
+}
